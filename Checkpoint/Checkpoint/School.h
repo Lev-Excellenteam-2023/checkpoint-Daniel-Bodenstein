519699b117a96451c89b00b2b3272c724fff6031
Daniel Bodenstein <danielboden111@gmail.com>
2023-07-31 18:03:01 +0300

    structs

---
+++ Checkpoint/Checkpoint/School.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stddef.h>
+#include "Student.h"
+
+struct Students school[NUM_OF_LEVELS][NUM_OF_CLASSES];
+
+int schoolInit() {
+	for (int i = 0; i < NUM_OF_LEVELS; i++) {
+		for (int j = 0; j < NUM_OF_CLASSES; j++) {
+			school[i][j].head = NULL;
+			school[i][j].last = NULL;
+		}
+	}
+}
---
+++ Checkpoint/Checkpoint/Student.h
@@ -0,0 +1,20 @@
+#define NUM_OF_GRADES 10
+#define NUM_OF_LEVELS 12
+#define NUM_OF_CLASSES 10
+#define MAX_SIZE_OF_NAME 50
+
+struct Student {
+    char firstName[MAX_SIZE_OF_NAME];
+    char lastName[MAX_SIZE_OF_NAME];
+    long long int phone;
+    int level;
+    int classroom;
+    int grades[NUM_OF_GRADES];
+    struct Student* next;
+};
+
+struct Students {
+    struct Student* head;
+    struct Student* last;
+};
+
